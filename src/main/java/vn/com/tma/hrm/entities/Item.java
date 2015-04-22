d32408a2f0ae7fb918784e28362e0ccc6d76989d
godfather508 <leducnam_1986@yahoo.com>
2015-04-22 23:45:30 +0700

    Initial Code

---
+++ src/main/java/vn/com/tma/hrm/entities/Item.java
@@ -0,0 +1,42 @@
+package vn.com.tma.hrm.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item")
+public class Item {
+
+  @Id
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
+  private int id;
+  
+  @Column
+  private boolean checked;
+  
+  @Column
+  private String description;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public boolean isChecked() {
+    return checked;
+  }
+
+  public void setChecked(boolean checked) {
+    this.checked = checked;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
---
+++ src/main/java/vn/com/tma/hrm/repository/ItemRepository.java
@@ -0,0 +1,11 @@
+package vn.com.tma.hrm.repository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import vn.com.tma.hrm.entities.Item;
+
+
+@RepositoryRestResource(collectionResourceRel = "items", path = "items")
+public interface ItemRepository extends PagingAndSortingRepository<Item, Integer> {
+
+}
