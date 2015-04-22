5b932e7c2bf054f1fa6e384170cfe9b9bd29a0b8
godfather508 <leducnam_1986@yahoo.com>
2015-04-22 23:28:29 +0700

    Enter now

--- src/main/java/vn/com/tma/hrm/controller/TaskController.java
+++ src/main/java/vn/com/tma/hrm/controller/TaskController.java
@@ -2,13 +2,14 @@ package vn.com.tma.hrm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/task")
 public class TaskController {
 
-	@RequestMapping("/home")
-	public String home() {
-		return "index";
-	}
-
+	  @RequestMapping(method = RequestMethod.GET)
+	  public String viewApplication() {
+	    return "index";
+	  }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,8 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+	<h2>Hello World!</h2>
+	<br> This is HRM Tool
+	<br>
+	<a href="/task">Enter now</a>
 </body>
 </html>
