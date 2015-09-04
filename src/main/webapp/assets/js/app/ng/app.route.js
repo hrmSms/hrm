0908fdbadfb61bf0d15413a78cd5dabecebe10d1
giangdinhtt <giangdinhtt@gmail.com>
2015-09-04 16:11:40 +0700

    Configure route

--- src/main/webapp/assets/js/app/ng/app.route.js
+++ src/main/webapp/assets/js/app/ng/app.route.js
@@ -3,6 +3,9 @@
  */
 'use strict';
 angular.module('hrmApp.route', [])
+.config(['$urlMatcherFactoryProvider', function($urlMatcherFactoryProvider){
+  $urlMatcherFactoryProvider.strictMode(false);
+}])
 .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider){
   // Redirect to '/' in case of unmatched url
   $urlRouterProvider.otherwise('/');
@@ -13,6 +16,18 @@ angular.module('hrmApp.route', [])
       url: '/blank',
       templateUrl: 'pages/blank.html',
       controller: ''
-    })
-    
+    });
+
+  //https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions#how-to-make-a-trailing-slash-optional-for-all-routes
+  $urlRouterProvider.rule(function ($injector, $location) {
+    var path = $location.url();
+    // check to see if the path already has a slash where it should be
+    if (path[path.length - 1] === '/' || path.indexOf('/?') > -1) {
+        return;
+    }
+    if (path.indexOf('?') > -1) {
+        return path.replace('?', '/?');
+    }
+    return path + '/';
+  });
 }]);
