17a3fe247fe260b851ac5b3fc006578d5f37a898
Nguyen huy <huynqhttt@gmail.com>
2015-09-11 12:02:13 +0700

    Update Sprint create input form

--- src/main/webapp/assets/js/app/ng/pages/sprint-controller.js
+++ src/main/webapp/assets/js/app/ng/pages/sprint-controller.js
@@ -1,9 +1,39 @@
 /**
- * Controller of Sprint
- * Author: nqhuy1@tma.com.vn
+ * Controller of Sprint Author: nqhuy1@tma.com.vn
  */
 'use strict';
-angular.module('hrmApp.controllers')
-.controller('SprintCtrl', ['$scope', function($scope) {
-	
-}]);
+angular.module('hrmApp.controllers').controller('SprintCtrl', [ '$scope', function($scope) {
+
+  $scope.sprintID = null;
+  $scope.active = 1;
+  $scope.actuals = null;
+  $scope.description = null;
+  $scope.endDate = null;
+  $scope.name = null;
+  $scope.note = null;
+  $scope.planEst = null;
+  $scope.planVelocity = null;
+  $scope.project = {};
+  $scope.sprintstate = {};
+  $scope.startDate = null;
+  $scope.taskEst = null;
+  $scope.toDo = null;
+
+  $scope.sprint = function(id) {
+
+    this.id = id;
+    this.active = null;
+    this.actuals = null;
+    this.description = null;
+    this.endDate = null;
+    this.name = null;
+    this.note = null;
+    this.planEst = null;
+    this.planVelocity = null;
+    this.project = null;
+    this.sprintstate = null;
+    this.startDate = null;
+    this.taskEst = null;
+    this.toDo = null;
+  };
+} ]);
