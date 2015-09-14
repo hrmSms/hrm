516df057ee364ec73bf011985c5ca444cedf3320
nlonguit <nlonguit@gmail.com>
2015-09-14 18:30:50 +0700

    security
    security

--- src/main/java/vn/com/tma/hrm/services/UserService.java
+++ src/main/java/vn/com/tma/hrm/services/UserService.java
@@ -6,9 +6,7 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import vn.com.tma.hrm.entities.User;
-import vn.com.tma.hrm.entities.UserCreateForm;
-import vn.com.tma.hrm.entities.UserRegistrationForm;
-import vn.com.tma.hrm.entities.VerificationToken;
+
 
 @Service
 public interface UserService {
@@ -19,15 +17,5 @@ public interface UserService {
     Optional<User> getUserByUsername(String username);
 
     Collection<User> getAllUsers();
-
-    User create(UserCreateForm form);
-
-    User registerNewUserAccount(UserRegistrationForm form);
-
-    User saveRegisteredUser(User user);
-
-    void createVerificationTokenForUser(User user, String token);
-
-    VerificationToken getVerificationToken(String VerificationToken);
 
 }
