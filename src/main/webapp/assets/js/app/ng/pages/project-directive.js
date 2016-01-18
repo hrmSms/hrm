9504145cdb9dfae336bf699ff01e5e4f8b823331
tri-duongminh <duongminhtri@gmail.com>
2016-01-18 14:41:55 +0700

    .

---
+++ src/main/webapp/assets/js/app/ng/pages/project-directive.js
@@ -0,0 +1,38 @@
+'use strict';
+angular.module('hrmApp.directives').directive('projectnameValidator', function($q, $timeout, $http, ApiConfigs) {
+
+  return {
+    restrict : 'A',
+    require : 'ngModel',
+    link : function(scope, element, attrs, ngModel) {
+        ngModel.$asyncValidators.projectname = function(modelValue, viewValue) {
+            if (!viewValue) {
+                return $q.when(true);
+            }
+            var deferred = $q.defer();
+            
+            var urlBase = ApiConfigs.Url.PROJECT;
+        	urlBase += '/search/findByName?name=';        	        	
+        	var isUnique = false;
+        	$http.get(urlBase + viewValue)
+            .success(function (data) {
+                if (data._embedded != undefined) {
+//                	console.log('Line 19:' + data._embedded.projects[0].name);
+                    isUnique = true;
+                }        	                
+            });
+        	
+        	
+            $timeout(function() {
+                if (viewValue && isUnique) {
+                    deferred.reject();
+                }
+
+                deferred.resolve();
+            }, 500);
+            return deferred.promise;
+        };
+    }
+  };
+
+} )
