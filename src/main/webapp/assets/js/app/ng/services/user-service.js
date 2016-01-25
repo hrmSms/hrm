5c585e88ec8c08b293160f6887e57335f4d3c50f
nlonguit <nlonguit@gmail.com>
2016-01-25 16:28:29 +0700

    update user-service
    update user-service

--- src/main/webapp/assets/js/app/ng/services/user-service.js
+++ src/main/webapp/assets/js/app/ng/services/user-service.js
@@ -4,11 +4,19 @@
 angular.module('hrmApp.services')
 	.factory('UserService', ['$http', '$q', function($http, $q) {
 		var user={};
-
 		user.getAuthentication = function() {
 			var deferred = $q.defer();
 			$http.get('user').success(function(data) {
-				deferred.resolve(data);
+				console.log('data ' + JSON.stringify(data));
+				deferred.resolve({
+					authenticated : data.authenticated,
+					username : data.principal.user.username,
+					email : data.principal.user.email,
+					nonExpired : data.principal.user.nonExpired,
+					id : data.principal.user.id,
+					role : data.principal.user.role,
+					enabled : data.principal.user.enabled
+				});
 			}).error(function() {
 				deferred.reject('There was an error');
 			})
